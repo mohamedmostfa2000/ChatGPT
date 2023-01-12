49a11c712795b049dcb012dee4b8ede2892bf160
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2023-01-12 06:34:14 +0200

    Mohamed Mostfa - API function implementation

---
+++ lib/api_key.dart
@@ -0,0 +1,1 @@
+const apiSecretKey = "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";
--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'api_key.dart';
 import 'chat_message_widget.dart';
 import 'colors.dart';
 import 'model.dart';
+import 'package:http/http.dart' as http;
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key, required String title});
@@ -20,6 +24,31 @@ class _ChatPageState extends State<ChatPage> {
   void initState() {
     super.initState();
     isLoading = false;
+  }
+
+  Future<String> generateResponse(String prompt) async {
+    final apiKey = apiSecretKey;
+
+    var url = Uri.https("api.openai.com", "/v1/completions");
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey'
+      },
+      body: jsonEncode({
+        "model": "text-davinci-003",
+        "prompt": prompt,
+        'temperature': 0,
+        'max_tokens': 2000,
+        'top_p': 1,
+        'frequency_penalty': 0.0,
+        'presence_penalty': 0.0,
+      }),
+    );
+
+    Map<String, dynamic> newresponse = jsonDecode(response.body);
+    return newresponse['choices'][0]['text'];
   }
 
   @override
